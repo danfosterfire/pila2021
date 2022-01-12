b15df90b7293e2f6c8dc134705b29409f62042ce
danfosterfire <danfoster@berkeley.edu>
2022-01-12 09:55:48 -0800

    finish SBC setup

--- 03-analysis/04-sim_based_calibration.R
+++ 03-analysis/04-sim_based_calibration.R
@@ -8,17 +8,12 @@
 
 #### setup #####################################################################
 
+
 library(here)
 library(tidyverse)
 library(cmdstanr)
 library(bayesplot)
 library(posterior)
-
-# load observed data
-pila_data = readRDS(here::here('02-data', '02-for_analysis', 'pila_data.rds'))
-
-# compile the SBC code
-SBC_model = cmdstan_model(here::here('03-analysis', 'SBC_model.stan'))
 
 
 #### simulate, estimate, rank ##################################################
@@ -26,9 +21,15 @@ SBC_model = cmdstan_model(here::here('03-analysis', 'SBC_model.stan'))
 sim_est_rank = 
   function(id){
     
+    library(here)
+    library(tidyverse)
+    library(cmdstanr)
+    library(bayesplot)
+    library(posterior)
+    
     # load the observed data and the stan model
     sim_data = readRDS(here::here('02-data', '02-for_analysis', 'pila_data.rds'))
-    stan_model = cmdstan_model(here::here('03-analysis','model_noRanEff.stan'))
+    stan_model = cmdstanr::cmdstan_model(here::here('03-analysis','model_simple.stan'))
     
     # pull some true parameter values (these are normally pulled from the 
     # prior distribution, but the prior for beta_g[2] (starting size) is 
@@ -37,11 +38,12 @@ sim_est_rank =
     # include this narrow prior in the parameter estimation code because it 
     # would be a hassle to split out the fixeff for size from the rest of the 
     # fixed effects)
-    true_beta_s = rnorm(n = 14, mean = 0, sd = 1)
-    true_beta_g = rnorm(n = 14, mean = 0, sd = 1)
-    true_beta_g[2] = rnorm(n = 1, mean = 1, sd = 0.1)
-    true_sigmaEpsilon_g = rcauchy(n = 1, location = 0, scale = 1)
-    
+    true_beta_s = rnorm(n = 14, mean = 0, sd = 5)
+    true_beta_g = rnorm(n = 14, mean = 0, sd = 5)
+    true_sigmaEpsilon_g = 0
+    while (true_sigmaEpsilon_g <= 0){
+      true_sigmaEpsilon_g = rcauchy(n = 1, location = 0, scale = 5)
+    }
     # simulate some fake random effects
     
     # simulate some fake responses
@@ -76,16 +78,92 @@ sim_est_rank =
             list(beta_s = rep(0, times = 14),
                  beta_g = c(0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
                  sigmaEpsilon_g = 1)),
-        parallel_chains = 4,
+        parallel_chains = 1,
         output_dir = here::here('02-data', '03-results', 'sim_fits'),
-        output_basename = paste0('simfit_', id))
+        output_basename = paste0('simfit_', id),
+        thin = 4,
+        iter_warmup = 1000,
+        iter_sampling = 1000)
     
     sim_fit$save_object(here::here('02-data', '03-results', 'sim_fits',
                                    paste0('simfit_',id,'.rds')))
     
     # compare posterior samples against true parameter values
-    posterior_samples = as_draws_df(sim_fit$draws())
+    parameter_ranks = 
+      
+      # start with the (thinned) posterior samples
+      as_draws_df(sim_fit$draws()) %>%
+      
+      # create indicator columns comparing posterior samples to true values
+      mutate(
+        Rbeta01_s = as.integer(`beta_s[1]` < true_beta_s[1]),
+        Rbeta02_s = as.integer(`beta_s[2]` < true_beta_s[2]),
+        Rbeta03_s = as.integer(`beta_s[3]` < true_beta_s[3]),
+        Rbeta04_s = as.integer(`beta_s[4]` < true_beta_s[4]),
+        Rbeta05_s = as.integer(`beta_s[5]` < true_beta_s[5]),
+        Rbeta06_s = as.integer(`beta_s[6]` < true_beta_s[6]),
+        Rbeta07_s = as.integer(`beta_s[7]` < true_beta_s[7]),
+        Rbeta08_s = as.integer(`beta_s[8]` < true_beta_s[8]),
+        Rbeta09_s = as.integer(`beta_s[9]` < true_beta_s[9]),
+        Rbeta10_s = as.integer(`beta_s[10]` < true_beta_s[10]),
+        Rbeta11_s = as.integer(`beta_s[11]` < true_beta_s[11]),
+        Rbeta12_s = as.integer(`beta_s[12]` < true_beta_s[12]),
+        Rbeta13_s = as.integer(`beta_s[13]` < true_beta_s[13]),
+        Rbeta14_s = as.integer(`beta_s[14]` < true_beta_s[14]),
+        
+        Rbeta01_g = as.integer(`beta_g[1]` < true_beta_g[1]),
+        Rbeta02_g = as.integer(`beta_g[2]` < true_beta_g[2]),
+        Rbeta03_g = as.integer(`beta_g[3]` < true_beta_g[3]),
+        Rbeta04_g = as.integer(`beta_g[4]` < true_beta_g[4]),
+        Rbeta05_g = as.integer(`beta_g[5]` < true_beta_g[5]),
+        Rbeta06_g = as.integer(`beta_g[6]` < true_beta_g[6]),
+        Rbeta07_g = as.integer(`beta_g[7]` < true_beta_g[7]),
+        Rbeta08_g = as.integer(`beta_g[8]` < true_beta_g[8]),
+        Rbeta09_g = as.integer(`beta_g[9]` < true_beta_g[9]),
+        Rbeta10_g = as.integer(`beta_g[10]` < true_beta_g[10]),
+        Rbeta11_g = as.integer(`beta_g[11]` < true_beta_g[11]),
+        Rbeta12_g = as.integer(`beta_g[12]` < true_beta_g[12]),
+        Rbeta13_g = as.integer(`beta_g[13]` < true_beta_g[13]),
+        Rbeta14_g = as.integer(`beta_g[14]` < true_beta_g[14]),
+        
+        RsigmaEpsilon_g = as.integer(sigmaEpsilon_g < true_sigmaEpsilon_g)
+      ) %>%
+      
+      select(contains(c('Rbeta','RsigmaEpsilon'))) %>%
+      
+      # sum the indicators to get the rank for each variable
+      summarise_all(sum)
+    
+    return(parameter_ranks)
+    
     
   }
 
+library(foreach)
+library(doParallel)
+
+registerDoParallel(12)
+
+batch_param_ranks = 
+  foreach(i = 1:100) %dopar% {
+    sim_est_rank(i)
+  }
+
+saveRDS(batch_param_ranks,
+        here::here('02-data','03-results', 'sim_fits', 'batch_param_ranks.rds'))
+
+stopImplicitCluster()
+
 #### check uniformity of each parameter ########################################
+
+batch_param_ranks = 
+  do.call('bind_rows',
+          batch_param_ranks)
+
+batch_param_ranks %>%
+  rowid_to_column('run') %>%
+  pivot_longer(cols = c(-run), names_to = 'param', values_to = 'rank') %>%
+  ggplot(aes(x = rank))+
+  geom_histogram(scale = 'free')+
+  facet_wrap(~param)+
+  theme_minimal()
---
+++ 03-analysis/model_simple.stan
@@ -0,0 +1,49 @@
+
+data {
+  int<lower=1> K; // number of fixeff covariates, including intercept
+  
+  // survival data
+  int<lower=0> N_s; // number of responses for survival data
+  matrix[N_s,K] X_s; // fixeff covariates for survival data
+  int<lower=0, upper=1> surv[N_s]; // observed survival
+  
+  // growth data
+  int<lower=0> N_g; // number of responses for growth data
+  matrix[N_g,K] X_g; // fixeff covariates for growth data
+  real<lower=0> size1_g[N_g]; // size
+  
+}
+
+
+parameters {
+  vector[K] beta_s;
+  vector[K] beta_g;
+  real<lower=0> sigmaEpsilon_g;
+}
+
+transformed parameters {
+  
+}
+
+model {
+  // variable declarations
+  vector[N_s] XB_s;
+  vector[N_g] XB_g;
+  
+  // priors
+  beta_s ~ normal(0,5);
+  beta_g ~ normal(0,5);
+  sigmaEpsilon_g ~ normal(0,5);
+  
+  // random effect realizations
+  
+  // linear predictors
+  XB_s = X_s * beta_s;
+  XB_g = X_g * beta_g;
+  
+  // likelihoods
+  surv ~ bernoulli_logit(XB_s);
+  for (i in 1:N_g){
+    size1_g[i] ~ normal(XB_g[i], sigmaEpsilon_g)T[0,];
+  }
+}
