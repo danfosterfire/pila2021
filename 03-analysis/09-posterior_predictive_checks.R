51f1c8486e05b7db89796bc20726fe35d4c4323b
danfosterfire <danfoster@berkeley.edu>
2022-01-18 09:57:45 -0800

    pila model passes predictive checks

--- 03-analysis/09-posterior_predictive_checks.R
+++ 03-analysis/09-posterior_predictive_checks.R
@@ -1,6 +1,6 @@
 library(here)
 library(tidyverse)
-
+library(posterior)
 fitted_model = readRDS(here::here('02-data',
                                   '03-results',
                                   'real_fits',
@@ -53,15 +53,19 @@ mort_predictions =
   mutate(r = dense_rank(p.50),
          r_bin = cut(r, breaks = seq(0, nrow(.)+1, length.out = 20)))
 
-head(mort_retrodictions)
+head(mort_predictions)
 ggplot(
-  data = mort_retrodictions,
+  data = mort_predictions,
   aes(x = r, y = surv_true))+
   geom_jitter(height = 0.1, width = 0, size = 0, color = 'red')+
-  theme_minimal()+
-  geom_point(data = mort_retrodictions,
-             aes(x = r, y = p.50), size = 0)+
-  geom_ribbon(data = mort_retrodictions %>%
+  theme_minimal()+  
+  geom_point(data = mort_predictions %>%
+               group_by(r_bin) %>%
+               summarise(r = mean(r),
+                         p.50 = mean(p.50)) %>%
+               ungroup(),
+             aes(x = r, y = p.50))+
+  geom_ribbon(data = mort_predictions %>%
                 group_by(r_bin) %>%
                 summarise(r = mean(r),
                           p.50 = mean(p.50),
@@ -71,7 +75,7 @@ ggplot(
               aes(x = r, ymin = p.025, ymax = p.975, y = p.50),
               alpha = 0.2)+
   geom_point(data = 
-               mort_retrodictions %>% 
+               mort_predictions %>% 
                group_by(r_bin) %>%
                summarise(surv_true = sum(surv_true)/n(),
                          r = mean(r)) %>%
@@ -144,3 +148,225 @@ ggplot(data =
   geom_abline(intercept = 0, slope = 1, color = 'blue')
 
 # looks great
+
+#### recruitment ###############################################################
+
+
+samples.beta_f = samples %>% select(contains('beta_f'))
+samples.ecoEffect_f = samples %>% select(contains('ecoEFfect_f'))
+samples.plotEffect_f = samples %>% select(contains('plotEffect_f'))
+
+recruitment.predictions = 
+  do.call('bind_rows',
+          lapply(X = 1:nrow(samples),
+                 FUN = function(i){
+                   beta_f = as.numeric(samples.beta_f[i,])
+                   beta_g = as.numeric(samples.beta_g[i,])
+                   beta_s = as.numeric(samples.beta_s[i,])
+                   
+                   logf = 
+                     as.numeric(pila_validation$X_r %*% beta_f)+
+                     as.numeric(samples.ecoEffect_f[i,])[pila_validation$ecosub_r]+
+                     as.numeric(samples.plotEffect_f[i,])[pila_validation$plotid_r]
+                   
+                   mu = 
+                     as.numeric(pila_validation$X_r %*% beta_g)+
+                     as.numeric(samples.ecoEffect_g[i,])[pila_validation$ecosub_r]+
+                     as.numeric(samples.plotEffect_g[i,])[pila_validation$plotid_r]
+                   
+                   logitp = 
+                     as.numeric(pila_validation$X_r %*% beta_s)+
+                     as.numeric(samples.ecoEffect_s[i,])[pila_validation$ecosub_r]+
+                     as.numeric(samples.plotEffect_s[i,])[pila_validation$plotid_r]
+                   
+                   s = 
+                     matrix(nrow = 1, ncol = pila_validation$S_r, byrow = TRUE,
+                            data = 
+                              sapply(X = 1:pila_validation$S_r,
+                                     FUN = function(subplot){
+                                       boot::inv.logit(logitp[1+(pila_validation$M_r*(subplot-1))])
+                                     }))
+                   
+                   g = 
+                     matrix(nrow = 2, ncol = pila_validation$S_r, byrow = FALSE,
+                            data = 
+                              sapply(X = 1:pila_validation$S_r,
+                                     FUN = function(subplot){
+                                       sapply(X = 1:2,
+                                              FUN = function(sizeclass_to){
+                                                (pnorm(pila_validation$u_bounds[sizeclass_to],
+                                                      mean = mu[1+(pila_validation$M_r*(subplot-1))],
+                                                      sd = samples$sigmaEpsilon_g[i])-
+                                                   pnorm(pila_validation$l_bounds[sizeclass_to],
+                                                         mean = mu[1+(pila_validation$M_r*(subplot-1))],
+                                                         sd = samples$sigmaEpsilon_g[i])) / 
+                                                  (1-pnorm(0,
+                                                           mean = mu[1+(pila_validation$M_r*(subplot-1))],
+                                                           sd = samples$sigmaEpsilon_g[i]))
+                                              })
+                                     }))
+                   
+                   # growth kernel from sizeclass 1
+                   growKern = 
+                     matrix(nrow = pila_validation$S_r, ncol = 2, byrow = TRUE,
+                            data = 
+                              sapply(X = 1:pila_validation$S_r,
+                                     FUN = function(subplot){
+                                       sapply(X = 1:2,
+                                              FUN = function(sizeclass_to){
+                                                s[1,subplot]*g[sizeclass_to,subplot]
+                                              })
+                                     }))
+                   
+                   f = 
+                     matrix(nrow = pila_validation$M_r, ncol = pila_validation$S_r, 
+                            byrow = FALSE,
+                            data = 
+                              sapply(X = 1:pila_validation$S_r,
+                                     FUN = function(subplot){
+                                       sapply(X = 1:pila_validation$M_r,
+                                              FUN = function(sizeclass_from){
+                                                exp(logf[sizeclass_from+(pila_validation$M_r*(subplot-1))])
+                                              })
+                                     }))
+                   
+                   recKern = 
+                     array(dim = 
+                             c(pila_validation$S_r,
+                               2,
+                               pila_validation$M_r),
+                           dimnames = 
+                             list(subplot = 1:pila_validation$S_r,
+                                  sizeclass_to = 1:2,
+                                  sizeclass_from = 1:pila_validation$M_r),
+                           data = 
+                             sapply(X = 1:pila_validation$M_r,
+                                    FUN = function(sizeclass_from){
+                                      sapply(X = 1:2,
+                                             FUN = function(sizeclass_to){
+                                               sapply(X = 1:pila_validation$S_r,
+                                                      FUN = function(subplot){
+                                                        pila_validation$r[sizeclass_to]*
+                                                          f[sizeclass_from,subplot]
+                                                      })
+                                             })
+                                    }))
+                   
+                   A = 
+                     array(dim = 
+                             c(pila_validation$S_r,
+                               2,
+                               pila_validation$M_r),
+                           dimnames = 
+                             list(subplot = 1:pila_validation$S_r,
+                                  sizeclass_to = 1:2,
+                                  sizeclass_from = 1:pila_validation$M_r),
+                           data = 
+                             sapply(X = 1:pila_validation$M_r,
+                                    FUN = function(sizeclass_from){
+                                      sapply(X = 1:2,
+                                             FUN = function(sizeclass_to){
+                                               sapply(X = 1:pila_validation$S_r,
+                                                      FUN = function(subplot){
+                                                        if(sizeclass_from == 1){
+                                                          growKern[subplot, sizeclass_to]+
+                                                            recKern[subplot, sizeclass_to, sizeclass_from]
+                                                        } else {
+                                                          recKern[subplot,sizeclass_to,sizeclass_from]
+                                                        }
+                                                      })
+                                             })
+                                    }))
+                   
+                   nprime = 
+                     matrix(nrow = pila_validation$S_r, ncol = 2, byrow = TRUE,
+                            data = 
+                              sapply(X = 1:pila_validation$S_r,
+                                     FUN = function(subplot){
+                                       sapply(X = 1:2,
+                                              FUN = function(sizeclass){
+                                                as.numeric(A[subplot,sizeclass,] %*% 
+                                                             pila_validation$n[subplot,])
+                                              })
+                                     }))
+                   
+                   cprime_pred = 
+                     matrix(nrow = 2, ncol = pila_validation$S_r, byrow = FALSE,
+                            data = 
+                              sapply(X = 1:pila_validation$S_r,
+                                     FUN = function(subplot){
+                                       sapply(X = 1:2,
+                                              FUN = function(sizeclass){
+                                                rnbinom(n = 1,
+                                                        mu = 
+                                                          nprime[subplot,sizeclass]*
+                                                          pila_validation$a[sizeclass],
+                                                        size = samples$kappa_r[i])
+                                              })
+                                     }))
+                   
+                   result = 
+                     expand.grid(subplot = 1:pila_validation$S_r,
+                                 sizeclass = 1:2) %>%
+                     as_tibble() %>%
+                     arrange(subplot,sizeclass)
+                   
+                   result$density_pred = 
+                     sapply(X = 1:pila_validation$S_r,
+                            FUN = function(subplot){
+                              sapply(X = 1:2,
+                                     FUN = function(sizeclass){
+                                       nprime[subplot,sizeclass]*pila_validation$a[sizeclass]
+                                     })
+                            }) %>%
+                     as.numeric()
+                   result$count_sim = 
+                     sapply(X = 1:pila_validation$S_r,
+                            FUN = function(subplot){
+                              sapply(X = 1:2,
+                                     FUN = function(sizeclass){
+                                       cprime_pred[sizeclass,subplot]
+                                     })
+                            }) %>%
+                     as.integer()
+                   result$count_true = 
+                     sapply(X = 1:pila_validation$S_r,
+                            FUN = function(subplot){
+                              sapply(X = 1:2,
+                                     FUN = function(sizeclass){
+                                       pila_validation$cprime[sizeclass,subplot]
+                                     })
+                            }) %>%
+                     as.integer()
+                   result$iter = i
+                   return(result)
+                 }))
+
+head(recruitment.predictions)
+
+recruitment.predictions %>%
+  pivot_longer(cols = c('count_sim', 'count_true'),
+               names_to = 'source', values_to = 'count') %>%
+  ggplot(aes(x = count, fill = source))+
+  geom_bar(position = position_dodge())+
+  scale_x_continuous(limits = c(-1, 10))+
+  theme_minimal()
+
+# looks good
+
+recruitment.predictions %>%
+  group_by(subplot,sizeclass, count_true) %>%
+  summarise(density_pred.50 = quantile(density_pred,probs = 0.5),
+            count_sim.975 = quantile(count_sim, probs = 0.975),
+            count_sim.025 = quantile(count_sim, probs = 0.025),
+            count_sim.50 = quantile(count_sim, probs = 0.5)) %>%
+  ungroup() %>%
+  ggplot(aes(x = log(density_pred.50), y = count_sim.50))+
+  geom_line()+
+  geom_ribbon(aes(ymin = count_sim.025, ymax = count_sim.975),
+              alpha = 0.2)+
+  geom_point(aes(y = count_true))+
+  theme_minimal()
+
+
+# looks good
