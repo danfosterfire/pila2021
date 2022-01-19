7635a4b7529fdaae31c7c026887adaa5741fca65
danfosterfire <danfoster@berkeley.edu>
2022-01-18 16:41:35 -0800

    start building IPMs and estimating lambdas across posterior draws and subplot conditions

---
+++ 03-analysis/11-IPM.R
@@ -0,0 +1,347 @@
+
+library(here)
+library(tidyverse)
+library(posterior)
+library(bayesplot)
+
+# load mcmc results
+pila_fit = readRDS(here::here('02-data', '03-results', 'real_fits', 'pila.rds'))
+
+size_metadata = 
+  readRDS(here::here('02-data',
+                     '01-preprocessed',
+                     'size_metadata.rds')) %>%
+  # convert to metric
+  mutate(bin_midpoint = bin_midpoint * 0.0254,
+         bin_lower = bin_lower * 0.0254,
+         bin_upper = bin_upper * 0.0254,
+         dbh_m.mean = dbh_in.mean * 0.0254)
+
+size_metadata$r = 
+  c(readRDS(here::here('02-data',
+                   '02-for_analysis',
+                   'pila_training.rds'))$r,
+    rep(0, times = 18))
+
+head(size_metadata)
+
+subplots = 
+  readRDS(here::here('02-data', '01-preprocessed', 'subplot_data.rds'))%>%
+  mutate(ba_scaled = as.numeric(scale(ba_ft2ac)),
+         cwd_dep90_scaled = as.numeric(scale(cwd_departure90)),
+         cwd_mean_scaled = as.numeric(scale(cwd_mean)),
+         intercept = 1) %>%
+  select(subp_id, lat, lon, ecosubcd, intercept, fire, disease, ba_scaled, 
+         cwd_dep90_scaled,cwd_mean_scaled)
+
+posterior = as_draws_df(pila_fit$draws())
+
+posterior %>%
+  select(contains('beta_s')) %>%
+  slice(1) %>%
+  as.data.frame() %>%
+  as.numeric()
+
+explan_data =  
+  subplots %>%
+  expand(nesting(subp_id, lat, lon, ecosubcd, intercept, fire, disease,
+                 ba_scaled, cwd_dep90_scaled, cwd_mean_scaled),
+         nesting(size_metadata %>%
+                   select(dbh_m.mean))) 
+
+
+#### all real subplots (fixed effects only) ####################################
+A =
+  array(dim = c(nrow(size_metadata), # sizeclass to
+                nrow(size_metadata), # sizeclass from
+                100, # subplots
+                10), # posterior draws
+        dimnames = list('class_to' = 1:nrow(size_metadata),
+                        'class_from' = 1:nrow(size_metadata),
+                        'subplot' = 1:100,
+                        'draw' = 1:10),
+        data = 
+          sapply(X = 1:10,
+                 FUN = function(draw){
+                   
+                   # get beta_s for the current draw
+                   beta_s = 
+                     posterior %>%
+                     select(contains('beta_s')) %>%
+                     slice(draw) %>%
+                     as.data.frame() %>%
+                     as.numeric()
+                   
+                   beta_g = 
+                     posterior %>%
+                     select(contains('beta_g')) %>%
+                     slice(draw) %>%
+                     as.data.frame() %>%
+                     as.numeric()
+                   
+                   beta_f = 
+                     posterior %>%
+                     select(contains('beta_f')) %>%
+                     slice(draw) %>%
+                     as.data.frame() %>%
+                     as.numeric()
+                   
+                   sigmaEpsilon_g = 
+                     posterior %>%
+                     slice(draw) %>%
+                     pull(sigmaEpsilon_g) %>%
+                     as.numeric()
+                   
+                   sapply(X = 1:100,
+                          FUN = function(subplot){
+                            
+                            # construct explanatory variable matrix for survival 
+                            # for teh current subplot
+                            X = 
+                              subplots %>%
+                              slice(subplot) %>%
+                              expand(nesting(intercept, fire, disease, ba_scaled,
+                                             cwd_dep90_scaled,cwd_mean_scaled),
+                                     dbh = size_metadata$dbh_m.mean) %>%
+                              mutate(dbh_fire = dbh*fire,
+                                     dbh_disease = dbh*disease,
+                                     dbh_ba = dbh*ba_scaled,
+                                     dbh_cwd_dep90 = dbh*cwd_dep90_scaled,
+                                     dbh_cwd_mean = dbh*cwd_mean_scaled) %>%
+                              select(intercept, dbh, fire, disease, ba_scaled,
+                                     cwd_dep90_scaled, cwd_mean_scaled, 
+                                     dbh_fire, dbh_disease, dbh_ba,
+                                     dbh_cwd_dep90, dbh_cwd_mean) %>%
+                              as.matrix()
+                            
+                            # calculate size_from length vector of survival 
+                            # probabilities on this subplot with this parameter draw
+                            p = boot::inv.logit(as.numeric(X %*% beta_s))
+                            
+                            mu = as.numeric(X %*% beta_g)
+                            
+                            f = exp(as.numeric(X %*% beta_f))
+                            
+                            sapply(X = 1:nrow(size_metadata),
+                                   FUN = function(class_from){
+                                     
+                                     g = 
+                                       ((pnorm(size_metadata$bin_upper,
+                                               mu[class_from],
+                                               sigmaEpsilon_g) - 
+                                           pnorm(size_metadata$bin_lower,
+                                                 mu[class_from],
+                                                 sigmaEpsilon_g))/
+                                          (1-pnorm(0,
+                                                mu[class_from],
+                                                sigmaEpsilon_g)))
+                                     
+                                     sapply(X = 1:nrow(size_metadata),
+                                            FUN = function(class_to){
+                                              
+                                              transition_prob = 
+                                                # survival of each from class
+                                                (p[class_from] *
+                                                # prob of growth from to
+                                                g[class_to]) +
+                                                # number of new recruits
+                                                (f[class_from] *
+                                                 size_metadata$r[class_to])
+                                              return(transition_prob)
+                                              
+                                              # for testing
+                                              #paste0('d:',draw,'|s:',subplot,
+                                              #  '|f:',class_from,'|t:',class_to)
+                                            })
+                                   })
+                            })
+                 }))
+
+test = eigen(A[,,1,1])
+
+as.numeric(test$values)
+
+lambdas_full = 
+  sapply(X = 1:dim(A)[3],
+         FUN = function(subplot){
+           sapply(X = 1:dim(A)[4],
+                FUN = function(draw){
+                  max(as.numeric(eigen(as.matrix(A[,,subplot,draw]))$values))
+                })
+         }) %>%
+  as.numeric()
+
+lambdas_full
+ggplot(data = 
+         data.frame(lambda = lambdas_full),
+       aes(x = lambda))+
+  geom_density()+
+  scale_x_continuous(limits = c(0, 2))
+
+#### all real subplots (fixed and simulated random effects) ####################
+
+
+#### only fecundity subplots (fixed and random effects) ########################
+
+
+
+#### using hypothetical subplots (only fixed effects) ##########################
+
+head(subplots)
+
+hypothetical_subplots = 
+  data.frame(intercept = rep(1, times = 9),
+             fire = c(FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE),
+             disease = c(FALSE, FALSE, T, F, F, F, F, F, F),
+             ba_scaled = c(0, 0, 0, -1, 1, 0, 0, 0, 0),
+             cwd_dep90_scaled = c(0, 0, 0, 0, 0, -1, 1, 0, 0),
+             cwd_mean_scaled = c(0, 0, 0, 0, 0, 0, 0, -1, 1),
+             subp_id = 1:9,
+             name = c('Undisturbed', 'Fire', 'Disease', 'Low BA', 'High BA',
+                      'Low Drought', 'High Drought', 'Wet Site', 'Dry Site'))
+
+
+A_hypotheticals = 
+  array(dim = c(nrow(size_metadata), # sizeclass to
+                nrow(size_metadata), # sizeclass from
+                nrow(hypothetical_subplots), # subplots
+                400), # posterior draws
+        dimnames = list('class_to' = 1:nrow(size_metadata),
+                        'class_from' = 1:nrow(size_metadata),
+                        'subplot' = 1:nrow(hypothetical_subplots),
+                        'draw' = 1:400),
+        data = 
+          sapply(X = 1:400,
+                 FUN = function(draw){
+                   
+                   # get beta_s for the current draw
+                   beta_s = 
+                     posterior %>%
+                     select(contains('beta_s')) %>%
+                     slice(draw) %>%
+                     as.data.frame() %>%
+                     as.numeric()
+                   
+                   beta_g = 
+                     posterior %>%
+                     select(contains('beta_g')) %>%
+                     slice(draw) %>%
+                     as.data.frame() %>%
+                     as.numeric()
+                   
+                   beta_f = 
+                     posterior %>%
+                     select(contains('beta_f')) %>%
+                     slice(draw) %>%
+                     as.data.frame() %>%
+                     as.numeric()
+                   
+                   sigmaEpsilon_g = 
+                     posterior %>%
+                     slice(draw) %>%
+                     pull(sigmaEpsilon_g) %>%
+                     as.numeric()
+                   
+                   sapply(X = 1:nrow(hypothetical_subplots),
+                          FUN = function(subplot){
+                            
+                            # construct explanatory variable matrix for survival 
+                            # for the current subplot
+                            X = 
+                              hypothetical_subplots %>%
+                              slice(subplot) %>%
+                              expand(nesting(intercept, fire, disease, ba_scaled,
+                                             cwd_dep90_scaled,cwd_mean_scaled),
+                                     dbh = size_metadata$dbh_m.mean) %>%
+                              mutate(dbh_fire = dbh*fire,
+                                     dbh_disease = dbh*disease,
+                                     dbh_ba = dbh*ba_scaled,
+                                     dbh_cwd_dep90 = dbh*cwd_dep90_scaled,
+                                     dbh_cwd_mean = dbh*cwd_mean_scaled) %>%
+                              select(intercept, dbh, fire, disease, ba_scaled,
+                                     cwd_dep90_scaled, cwd_mean_scaled, 
+                                     dbh_fire, dbh_disease, dbh_ba,
+                                     dbh_cwd_dep90, dbh_cwd_mean) %>%
+                              as.matrix()
+                            
+                            # calculate size_from length vector of survival 
+                            # probabilities on this subplot with this parameter draw
+                            p = boot::inv.logit(as.numeric(X %*% beta_s))
+                            
+                            mu = as.numeric(X %*% beta_g)
+                            
+                            f = exp(as.numeric(X %*% beta_f))
+                            
+                            sapply(X = 1:nrow(size_metadata),
+                                   FUN = function(class_from){
+                                     
+                                     g = 
+                                       ((pnorm(size_metadata$bin_upper,
+                                               mu[class_from],
+                                               sigmaEpsilon_g) - 
+                                           pnorm(size_metadata$bin_lower,
+                                                 mu[class_from],
+                                                 sigmaEpsilon_g))/
+                                          (1-pnorm(0,
+                                                mu[class_from],
+                                                sigmaEpsilon_g)))
+                                     
+                                     sapply(X = 1:nrow(size_metadata),
+                                            FUN = function(class_to){
+                                              
+                                              transition_prob = 
+                                                # survival of each from class
+                                                (p[class_from] *
+                                                # prob of growth from to
+                                                g[class_to]) +
+                                                # number of new recruits
+                                                (f[class_from] *
+                                                 size_metadata$r[class_to])
+                                              return(transition_prob)
+                                              
+                                              # for testing
+                                              #paste0('d:',draw,'|s:',subplot,
+                                              #  '|f:',class_from,'|t:',class_to)
+                                            })
+                                   })
+                            })
+                 }))
+
+hypothetical_lambdas = 
+  hypothetical_subplots %>%
+  expand(nesting(subp_id, name, intercept, fire, disease, ba_scaled, cwd_dep90_scaled, 
+                 cwd_mean_scaled),
+         data.frame(draw = 1:400))
+
+hypothetical_lambdas$lambda = 
+  sapply(X = 1:nrow(hypothetical_subplots),
+         FUN = function(subplot){
+           sapply(X = 1:400,
+                  FUN = function(draw){
+                    # paste0('s:',subplot,'d:',draw) for testing
+                    max(as.numeric(eigen(A_hypotheticals[,,subplot,draw])$values))
+                  })
+         }) %>%
+  as.numeric()
+
+
+hypothetical_lambdas
+
+pretty_names = 
+  hypothetical_subplots$name
+names(pretty_names) = hypothetical_subplots$subp_id
+
+
+ggplot(data = 
+         hypothetical_lambdas,
+       aes(x = lambda))+
+  geom_density(lwd = 1)+
+  geom_vline(xintercept = 1, color = 'grey', lty = 2, lwd = 1)+
+  theme_minimal()+
+  facet_grid(subp_id~., scales = 'free_y',
+             labeller = labeller(subp_id = pretty_names))+
+  coord_cartesian(xlim = c(0.9,1.5))
+
+#### using hypothetical subplots (fixed and random effects) ####################
+
+
